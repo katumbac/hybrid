3bb163984493b336d7315abd50bb6d55f89d76bc
katumbac <katumbac@espol.edu.ec>
2024-01-15 11:13:52 -0500

    CORRECIONES

--- src/app/tab3/tab3.page.spec.ts
+++ src/app/tab3/tab3.page.spec.ts
@@ -2,11 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Tab3Page } from './tab3.page';
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> b9ed1b09bcae42a17d83bf8484e86a2d2b0b1495
 describe('Tab3Page', () => {
   let component: Tab3Page;
   let fixture: ComponentFixture<Tab3Page>;
